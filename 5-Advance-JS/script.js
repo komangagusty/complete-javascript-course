b18f7f3d6fb59ae86a22abf79d4c67a96badb164
Komang <komang@Macbookpros-MacBook-Pro.local>
2019-01-02 05:14:26 +0700

    primitive vs object

--- 5-Advance-JS/script.js
+++ 5-Advance-JS/script.js
@@ -62,3 +62,41 @@ var jane = Object.create(personProto,
 
 // Primitive vs Objects
 
+// Primitive
+var a = 23;
+var b = a;
+
+a = 46;
+
+console.log(a);
+console.log(b);
+
+// Object
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Function
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco'
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
